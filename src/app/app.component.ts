46d95c9c35a1754d8eb67cda8ff91c263b1f0c72
xnguyena2 <xnguyena2@yahoo.com>
2021-02-20 21:57:34 +0700

    [add show more]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { BeerDetail } from './object/BeerDetail';
 import { SearchQuery } from './object/SearchQuery';
 import { APIService } from './services/api.service';
 
-import {AppService} from './services/app.service';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
@@ -20,57 +20,45 @@ export class AppComponent {
   faClose = faTimes;
   hostUrl = AppConfig.HostUrl;
 
-  visibilitySearchBar:string='';
+  visibilitySearchBar: string = '';
 
   isMobileMode = false;
 
-  hoverMenuClass:string[]=[""];
-
-  listResult:BeerDetail[] = [];
+  listResult: BeerDetail[] = [];
 
   constructor(private AppService: AppService,
-    private APIService:APIService){
-    this.AppService.registerHideProductMenu(this.hideProductMenu);
+    private APIService: APIService) {
+    //this.AppService.registerHideProductMenu(this.hideProductMenu);
   }
 
   onSearchEnter(value: string) {
   }
 
   onSearch(value: string) {
-    if(value == ''){
-      if(this.listResult!=[]){
+    if (value == '') {
+      if (this.listResult != []) {
         this.listResult = [];
       }
-    }else{
-      this.APIService.SearchBeer(new SearchQuery(value, 0, 10), result =>{
-        if(result){
+    } else {
+      this.APIService.SearchBeer(new SearchQuery(value, 0, 10), result => {
+        if (result) {
           this.listResult = result.result;
         }
       });
     }
   }
 
-  hideProductMenu(){
-    this.hoverMenuClass.push('hide-element')
-    setTimeout(() => {
-      const index = this.hoverMenuClass.indexOf('hide-element', 0);
-      if (index > -1) {
-        this.hoverMenuClass.splice(index, 1);
-     }
-    }, 300);
-  }
-
-  mobileShowSearch(){
+  mobileShowSearch() {
     console.log("show click");
 
     this.isMobileMode = true;
     this.visibilitySearchBar = 'visible';
   }
 
-  clearSearchText(){
-    this.listResult=[];
-    if(this.isMobileMode){
-      this.visibilitySearchBar='hidden';
+  clearSearchText() {
+    this.listResult = [];
+    if (this.isMobileMode) {
+      this.visibilitySearchBar = 'hidden';
     }
   }
 }

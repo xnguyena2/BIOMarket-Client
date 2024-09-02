b5fb2e1c5cd40bfa8b5eddd69df4ffbb25cba19a
xnguyena2 <xnguyena2@yahoo.com>
2024-09-02 22:01:16 +0700

    [correct data fix logo]

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'BIOMarket-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Trùm Biển | Hải Sản Cao Cấp');
+    expect(app.title).toEqual('SoDi -  Quản lý bán hàng');
   });
 
   it('should render title', () => {
